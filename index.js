c4d253857bc370bdf7001a378487253be22037f8
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-01-08 21:25:40 +0600

    added products api

--- index.js
+++ index.js
@@ -22,15 +22,19 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  const productsCollection=client.db('artisian').collection('products')
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+    app.get('/products',async(req,res)=>{
+     const result= await productsCollection.find().toArray();
+     res.send(result);
+})
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
+    
   }
 }
 run().catch(console.dir);
