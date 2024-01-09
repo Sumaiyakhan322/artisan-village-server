9bd1eda6738c957d2a991758bc8c451d41b18eb3
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2024-01-09 19:00:45 +0600

    added count api for total number of products

--- index.js
+++ index.js
@@ -24,12 +24,15 @@ const client = new MongoClient(uri, {
 async function run() {
   const productsCollection=client.db('artisian').collection('products')
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
+   
     app.get('/products',async(req,res)=>{
      const result= await productsCollection.find().toArray();
      res.send(result);
+     //for pagination
+     app.get('/productsCount',async(req,res)=>{
+      const count =await productsCollection.estimatedDocumentCount();
+      res.send({count})
+     })
 })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
